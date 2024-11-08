d7a34bff916fa995924561964ff5892718100c5f
unknown <douglasmaiatome>
2024-11-08 15:26:55 -0300

    first commit

---
+++ handler.js
@@ -0,0 +1,19 @@
+'use strict';
+const moment = require('moment')
+
+module.exports.logger = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Serverless CI/CD Deo',
+        version: "v1.0",
+        timestamp: moment().unix(),
+      },
+      null,
+      2
+    ),
+  };
+
+
+};
